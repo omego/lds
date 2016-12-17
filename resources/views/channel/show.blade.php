fe1b79587b1910051af1bc97ba60d9c2926c4428
Nicolas Perrenoud <nicolas.perrenoud@united-security-providers.ch>
2016-12-18 00:00:01 +0100

    Integrated bxSlider

--- resources/views/channel/show.blade.php
+++ resources/views/channel/show.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 <h1>{{ $channel->name }}</h1>
-<ul>
+<ul class="bxslider">
 	@foreach ($channel->slides->sortBy('updated_at') as $slide)
 		<li>
 			<h2>{{ $slide->title }}</h2>
@@ -13,4 +13,16 @@
 		</li>
 	@endforeach
 </ul>
+@endsection
+
+@section('script')
+    $(document).ready(function(){
+        $('.bxslider').bxSlider({
+            mode: 'fade',
+            speed: 500,
+            pause: 3000,
+            auto: true,
+            pager: false          
+        });
+    });
 @endsection
