1b6747840f838d2df9c9e379aa8821c567e50432
Nicolas Perrenoud <nicolas.perrenoud@united-security-providers.ch>
2016-12-17 23:03:58 +0100

    Disable Vue Javascript

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -15,22 +15,22 @@ require('bootstrap-sass');
  * using reactive data binding and reusable components. Vue's API is clean
  * and simple, leaving you to focus on building your next great project.
  */
-
+/*
 window.Vue = require('vue');
 require('vue-resource');
-
+*/
 /**
  * We'll register a HTTP interceptor to attach the "CSRF" header to each of
  * the outgoing requests issued by this application. The CSRF middleware
  * included with Laravel will automatically verify the header's value.
  */
-
+/*
 Vue.http.interceptors.push((request, next) => {
     request.headers.set('X-CSRF-TOKEN', Laravel.csrfToken);
 
     next();
 });
-
+*/
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
